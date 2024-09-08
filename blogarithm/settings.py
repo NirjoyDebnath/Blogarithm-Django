1d2245817f91d03514613081502d78fa180a945b
Nirjoy Debnath <nirjoy.debnath@cefalo.com>
2024-09-09 02:07:11 +0600

    Read users

--- blogarithm/settings.py
+++ blogarithm/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
     'rest_framework',
-    'auths',
     'customuser'
 ]
 
--- customuser/urls.py
+++ customuser/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('signup/', views.sign_up)
+    path('signup/', views.sign_up),
+    path('', views.get_users)
 ]
